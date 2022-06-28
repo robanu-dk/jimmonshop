c418ea268f8ff7f85fec3642962a5857f10795f9
jasmineyulis <103353290+jasmineyulis@users.noreply.github.com>
2022-06-28 10:01:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,17 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\SigninController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileDashboardController;
 use App\Http\Controllers\StakeholderController;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\EventDashboardController;
 use App\Http\Controllers\ProductDashboardController;
 use App\Http\Controllers\CategoryDashboardController;
+use App\Http\Controllers\DashboardUserEventController;
+use App\Http\Controllers\DashboardUserProfileController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\RegisterEventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,26 +52,56 @@ Route::post('/sign+in',[SigninController::class,'authenticate']);
 
 Route::post('/logout',[SigninController::class,'logout']);
 
-Route::get('/registration',[RegistrationController::class,'index']);
+Route::get('/registration',[RegistrationController::class,'index'])->middleware('guest');
 
 Route::post('/registration',[RegistrationController::class,'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::middleware('auth:admin')->group(function() {
 
-Route::get('/dashboard/categories/create', [CategoryDashboardController::class,'create']);
+    Route::get('/dashboard', [ProfileDashboardController::class, 'index']);
 
-Route::get('/dashboard/categories/{kategori:slug}', [CategoryDashboardController::class,'show']);
+    Route::get('/dashboard/{admin}/edit', [ProfileDashboardController::class, 'edit']);
 
-Route::get('/dashboard/categories/{kategori}/edit', [CategoryDashboardController::class,'edit']);
+    Route::put('/dashboard', [ProfileDashboardController::class, 'update']);
 
-Route::post('/dashboard/categories', [CategoryDashboardController::class,'store']);
+    Route::delete('/dashboard', [ProfileDashboardController::class, 'destroy']);
 
-Route::delete('/dashboard/categories/{kategori:slug}', [CategoryDashboardController::class,'destroy']);
+    Route::get('/dashboard/categories/create', [CategoryDashboardController::class,'create']);
 
-Route::put('/dashboard/categories/{kategori:slug}', [CategoryDashboardController::class,'update']);
+    Route::get('/dashboard/categories/{kategori:slug}', [CategoryDashboardController::class,'show']);
 
-Route::resource('/dashboard/categories', CategoryDashboardController::class);
+    Route::get('/dashboard/categories/{kategori}/edit', [CategoryDashboardController::class,'edit']);
 
-Route::resource('/dashboard/products', ProductDashboardController::class);
+    Route::post('/dashboard/categories', [CategoryDashboardController::class,'store']);
 
-Route::resource('/dashboard/events', EventDashboardController::class);
+    Route::delete('/dashboard/categories/{kategori:slug}', [CategoryDashboardController::class,'destroy']);
+
+    Route::put('/dashboard/categories/{kategori:slug}', [CategoryDashboardController::class,'update']);
+
+    Route::resource('/dashboard/categories', CategoryDashboardController::class);
+
+    Route::resource('/dashboard/products', ProductDashboardController::class);
+
+    Route::resource('/dashboard/events', EventDashboardController::class);
+
+    Route::get('/dashboard/events/{slug}/partisipant', [EventDashboardController::class, 'partisipant']);
+
+});
+
+Route::middleware('auth')->group(function() {
+
+    Route::resource('/dashboard/my/profile', DashboardUserProfileController::class);
+
+    Route::put('/dashboard/my/profile', [DashboardUserProfileController::class, 'update']);
+
+    Route::delete('/dashboard/my/profile', [DashboardUserProfileController::class, 'destroy']);
+
+    Route::get('/register+event/{event:slug}', [RegisterEventController::class, 'index']);
+
+    Route::post('/register+event/{event:slug}', [RegisterEventController::class, 'store']);
+
+    Route::resource('/dashboard/my/register_event', DashboardUserEventController::class);
+
+    Route::post('/product/{slug}/purchase', [PurchaseController::class, 'index']);
+
+});
